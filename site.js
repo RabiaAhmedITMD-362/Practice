c0470e9e24135bede245450f81775996c457979e
rsahmed16 <112486837+rsahmed16@users.noreply.github.com>
2023-03-03 17:27:32 -0600

    Create site.js

---
+++ site.js
@@ -0,0 +1,37 @@
+// Wait for the DOM to be fully loaded
+$(document).ready(function() {
+
+  // Get form elements
+  var nameField = $('#nameField input');
+  var emailField = $('#emailField input');
+  var passwordField = $('#passwordField input');
+  var signUpBtn = $('#signupBtn');
+  var signInBtn = $('#signinBtn');
+  
+  // Check form fields and enable/disable buttons accordingly
+  function checkFormFields() {
+    if (nameField.val() !== '' && emailField.val() !== '' && passwordField.val() !== '') {
+      signUpBtn.prop('disabled', false);
+      signInBtn.removeClass('disable');
+    } else {
+      signUpBtn.prop('disabled', true);
+      signInBtn.addClass('disable');
+    }
+  }
+  
+  // On keyup, check form fields
+  nameField.on('keyup', checkFormFields);
+  emailField.on('keyup', checkFormFields);
+  passwordField.on('keyup', checkFormFields);
+  
+  // On click, submit form
+  signUpBtn.on('click', function() {
+    alert('Thank you for signing up!');
+  });
+  
+  // On click, show sign in form
+  signInBtn.on('click', function() {
+    alert('Showing sign in form');
+  });
+
+});
